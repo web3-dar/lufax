065beafc6c76fc0d509e65aa55b249a368ea189b
web3-dar <onlineagentalmashell@gmail.com>
2024-11-19 22:56:13 +0100

    lo

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,19 +2,20 @@ import backgroundVideo from '../images/bg-vid.mp4'; // Replace with your video p
 
 const HeroSection = () => {
   return (
-    <section className="w-full min-h-screen">
+    <section className="relative w-full min-h-screen">
       {/* Background Video */}
       <video
         src={backgroundVideo}
         autoPlay
         loop
         muted
-        className="absolute top-0 left-0 w-full h-screen object-cover z-0"
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover z-0"
       ></video>
 
       {/* Hero Content */}
       <div className="absolute top-0 left-0 w-full min-h-screen flex justify-center items-center text-center z-10">
-        <h1 className="text-white text-3xl sm:text-3xl md:text-5xl font-semibold drop-shadow-lg max-w-[50%] tracking-wide leading-">
+        <h1 className="text-white text-3xl sm:text-3xl md:text-5xl font-semibold drop-shadow-lg max-w-[50%] tracking-wide leading-tight">
           A Leading Services Enabler For Small Business Owners In China
         </h1>
       </div>
